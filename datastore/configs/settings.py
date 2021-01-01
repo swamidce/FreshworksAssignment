16aa493f4b29260ff90d19306267764327c494cf
SWAMI NANDAN <53258898+swamidce@users.noreply.github.com>
2021-01-02 02:59:16 +0530

    Create settings.py

---
+++ datastore/configs/settings.py
@@ -0,0 +1,8 @@
+# Sets DEBUG = False when running on production server.
+DEBUG = True
+
+# Sets the host for the flask server.
+HOST = 'localhost'
+
+# Sets the port for the flask server.
+PORT = 5000
